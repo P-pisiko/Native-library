753ce68d376b4d8a4c408710463cc066b76b1ba7
P-pisiko <enesfidan75@proton.me>
2025-01-04 18:39:56 -0500

    Project completion

---
+++ src/app/bookservice.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Book {
+
+  title: string;
+  author: string;
+  price: number;
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class BookserviceService {
+
+  apiUrl = 'http://localhost:3000/books';
+
+  constructor(private http: HttpClient) { }
+
+  getAllBooks(): Observable<Book[]> {
+    return this.http.get<Book[]>(this.apiUrl);
+  }
+
+  createBook(book: Book): Observable<any> {
+    return this.http.post(this.apiUrl, book);
+  }
+
+  updateBook(book: Book): Observable<any> {
+    console.log(book);
+    console.log(book.title);
+    return this.http.put(`${this.apiUrl}/${book.title}`, book);
+  }
+
+  deleteBook(Title: string): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${Title}`);
+  }
+}
+
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,99 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { BookserviceService,Book } from '../bookservice.service';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  books: Book[] = [];
+  newBook: Book = {
+    title: '',
+    author: '',
+    price: 0,
+  };
+  isEditing = false;
 
+  constructor(private bookService: BookserviceService, private alertController: AlertController) {}
+
+  ngOnInit() {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.bookService.getAllBooks().subscribe({
+      next: (books) => {
+        this.books = books;
+        console.log(this.books);
+      },
+      error: (err) => {
+        this.showErrorAlert('Failed to load books', err);
+      }
+    });
+  }
+
+  saveBook() {
+    if (!this.newBook.title || !this.newBook.author || this.newBook.price <= 0) {
+      this.showErrorAlert('Invalid Input', 'Please fill all fields correctly');
+      return;
+  }
+
+  if (this.isEditing) {
+    // Update existing book
+    this.bookService.updateBook(this.newBook).subscribe({
+      next: () => {
+        this.loadBooks();
+        this.resetForm();
+      },
+      error: (err) => this.showErrorAlert('Update Failed', err)
+    });
+  }
+
+  else {
+    // Create new book
+    const bookToCreate = { ...this.newBook };
+
+    this.bookService.createBook(bookToCreate).subscribe({
+      next: () => {
+        this.loadBooks();
+        this.resetForm();
+      },
+      error: (err) => this.showErrorAlert('Create Failed', err)
+    });
+  }
+}
+  editBook(book: Book) {
+    this.newBook = { ...book };
+    this.isEditing = true;
+  }
+
+  deleteBook(title: string) {
+    this.bookService.deleteBook(title).subscribe({
+      next: () => {
+        this.loadBooks();
+      },
+      error: (err) => this.showErrorAlert('Delete Failed', err)
+    });
+  }
+
+  resetForm() {
+    this.newBook = {
+      title: '',
+      author: '',
+      price: 0,
+    };
+    this.isEditing = false;
+  }
+
+  async showErrorAlert(header: string, error: any) {
+    const alert = await this.alertController.create({
+      header: header,
+      message: error.message || 'An error occurred',
+      buttons: ['Ok :(']
+    });
+
+    await alert.present();
+  }
 }
