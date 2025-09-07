a9cb7215fe42e970100739ed55f354c22876ff91
P-pisiko <enesfidan75@proton.me>
2025-09-06 23:48:19 -0400

    minor cleanup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,7 @@ import { AlertController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage implements OnInit {
 
   books: Book[] = [];
